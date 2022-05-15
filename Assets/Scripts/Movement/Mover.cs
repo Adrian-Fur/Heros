9c32a1e1a8ac73d127d800e12c6590771ebcd67c
Adrian Furmanek <adrian.furm@gmail.com>
2022-05-15 23:27:38 +0200

    enemy prefab and player control

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -7,22 +7,12 @@ public class Mover : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetMouseButton(0) || Input.GetMouseButton(1))
-        {
-            MoveToCursor();
-        }
         UpdateAnimator();
     }
-
-    private void MoveToCursor()
+    
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
-        {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
+        GetComponent<NavMeshAgent>().destination = destination;
     }
 
     private void UpdateAnimator()
