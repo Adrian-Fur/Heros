1ee9f2b52582faece6cd7b723788f6fc152a22c4
Adrian Furmanek <adrian.furm@gmail.com>
2022-05-18 01:03:07 +0200

    Death animation when health is 0

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -6,11 +6,25 @@ namespace RPG.Combat
     {
         [SerializeField]
         float health = 100f;  
+        bool isDeath = false;
 
         public void TakeDamage(float damage)
         {
             health = Mathf.Max(health - damage, 0);
-            print(health);
-        }         
+            if (health == 0)
+            {
+                Die();
+            }
+
+        }
+
+        private void Die()
+        {
+            if (isDeath)
+                return;
+            
+            isDeath = true;
+            GetComponent<Animator>().SetTrigger("die");
+        }
     }
 }
